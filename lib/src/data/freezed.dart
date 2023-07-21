070e87c6a642beedb79515d2699702499669aec7
yuanjunyao <yuanjunyao@3c2p.com>
2023-07-22 07:16:13 +0800

    feat(json): 增加读取自身Json

--- lib/src/data/freezed.dart
+++ lib/src/data/freezed.dart
@@ -1,4 +1,9 @@
-/// 用于Freezed，把数据强制转成字符串，不会包空错误
+/// 用于json转换，把数据强制转成字符串，不会包空错误
 dynamic readAsString(Map<dynamic, dynamic> json, String key) {
   return json[key].toString();
+}
+
+/// 用于json转换，把json自身返回
+dynamic readSelf(Map<dynamic, dynamic> json, String key) {
+  return json;
 }
